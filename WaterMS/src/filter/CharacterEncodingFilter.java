edec49457d934b81c56825f1fcadb587941bbd6d
Administrator <Administrator@DESKTOP-P7ACBD1>
2016-04-18 20:57:01 +0800

    实现编码过滤器类

--- WaterMS/src/filter/CharacterEncodingFilter.java
+++ WaterMS/src/filter/CharacterEncodingFilter.java
@@ -8,26 +8,34 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class CharacterEncodingFilter implements Filter {
-
+	protected String encoding = "UTF-8";
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-
+		this.encoding = null;
 	}
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		// 设置request编码
+		request.setCharacterEncoding(this.encoding);
+		// 设置相应信息
+		response.setContentType("text/html;charset=" + this.encoding);
+		response.setCharacterEncoding(this.encoding);
+		chain.doFilter(new CharacterEncodingRequest(request), response);
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-
+	public void init(FilterConfig fConfig) throws ServletException {
+		String encoding = fConfig.getInitParameter("encoding");
+		if (encoding != null)
+			this.encoding = encoding;
 	}
 
 }
