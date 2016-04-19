4bdcde9dc74c413ef8fbfa5a0cb826c60ebaad22
Administrator <Administrator@DESKTOP-P7ACBD1>
2016-04-19 15:00:29 +0800

    实现获取所有的发文函数

--- WaterMS/src/impl/cm/DocSendImpl.java
+++ WaterMS/src/impl/cm/DocSendImpl.java
@@ -1,5 +1,41 @@
 package impl.cm;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.DBHelper;
+import entity.DocSend;
+
 public class DocSendImpl {
+
+	public List<DocSend> fetchAll() {
+		Connection conn = DBHelper.getConn();
+		String sql = "select * from doc_send";
+		ResultSet rs = DBHelper.getRS(conn, sql);
+		List<DocSend> list = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				DocSend docSend = new DocSend();
+				docSend.setId(rs.getString("doc_id"));
+				docSend.setTitle(rs.getString("doc_title"));
+				docSend.setCont(rs.getString("doc_cont"));
+				docSend.setSecret(rs.getString("doc_secret"));
+				docSend.setType(rs.getString("Type_id"));
+				docSend.setUrgency(rs.getString("doc_urgency"));
+				docSend.setDraftTime(rs.getDate("doc_draftTime").getTime());
+				docSend.setDrafter(rs.getString("doc_drafter"));
+				docSend.setChecker(rs.getString("doc_checker"));
+				docSend.setState(rs.getString("doc_state"));
+				list.add(docSend);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DBHelper.close(conn);
+		return list;
+	}
 
 }
