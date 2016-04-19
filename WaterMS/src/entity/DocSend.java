551f141347e59eb670ff886d2121c50b091a3c5d
Administrator <Administrator@DESKTOP-P7ACBD1>
2016-04-19 13:59:42 +0800

    实现发文类

--- WaterMS/src/entity/DocSend.java
+++ WaterMS/src/entity/DocSend.java
@@ -1,5 +1,109 @@
 package entity;
 
 public class DocSend {
+	String id, title, cont, secret, type, urgency, datetime, drafter, sector, checker, state;
 
+	public DocSend(String id, String title, String cont, String secret, String type, String urgency, String datetime,
+			String drafter, String sector, String checker, String state) {
+		this.id = id;
+		this.title = title;
+		this.cont = cont;
+		this.secret = secret;
+		this.type = type;
+		this.urgency = urgency;
+		this.datetime = datetime;
+		this.drafter = drafter;
+		this.sector = sector;
+		this.secret = secret;
+		this.checker = checker;
+		this.state = state;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCont() {
+		return cont;
+	}
+
+	public void setCont(String cont) {
+		this.cont = cont;
+	}
+
+	public String getSecret() {
+		return secret;
+	}
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getUrgency() {
+		return urgency;
+	}
+
+	public void setUrgency(String urgency) {
+		this.urgency = urgency;
+	}
+
+	public String getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+
+	public String getDrafter() {
+		return drafter;
+	}
+
+	public void setDrafter(String drafter) {
+		this.drafter = drafter;
+	}
+
+	public String getSector() {
+		return sector;
+	}
+
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+
+	public String getChecker() {
+		return checker;
+	}
+
+	public void setChecker(String checker) {
+		this.checker = checker;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
 }
