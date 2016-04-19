a00994482c5e590700666e29fb4700be8a3d080d
Administrator <Administrator@DESKTOP-P7ACBD1>
2016-04-19 14:03:31 +0800

    实现公文类型数据交互类

--- WaterMS/src/impl/cm/DocTypeImpl.java
+++ WaterMS/src/impl/cm/DocTypeImpl.java
@@ -1,5 +1,35 @@
 package impl.cm;
 
-public class DocTypeImpl {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import db.DBHelper;
+import entity.DocType;
+
+public class DocTypeImpl {
+	/**
+	 * 获取所有的记录
+	 * @return
+	 */
+	public List<DocType> fetchAll() {
+		Connection conn = DBHelper.getConn();
+		String sql = "select * from doc_type";
+		ResultSet rs = DBHelper.getRS(conn, sql);
+		List<DocType> list = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				DocType docType = new DocType();
+				docType.setType_id(rs.getString("type_id"));
+				docType.setType_cont(rs.getString("type_cont"));
+				list.add(docType);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DBHelper.close(conn);
+		return list;
+	}
 }
