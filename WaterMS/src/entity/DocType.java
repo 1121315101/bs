301e12284c999c41b74464ea82a78a5d51547c87
Administrator <Administrator@DESKTOP-P7ACBD1>
2016-04-19 13:58:55 +0800

    实现公文类型类

--- WaterMS/src/entity/DocType.java
+++ WaterMS/src/entity/DocType.java
@@ -1,5 +1,21 @@
 package entity;
 
 public class DocType {
+	String type_id, type_cont;
 
+	public String getType_id() {
+		return type_id;
+	}
+
+	public void setType_id(String type_id) {
+		this.type_id = type_id;
+	}
+
+	public String getType_cont() {
+		return type_cont;
+	}
+
+	public void setType_cont(String type_cont) {
+		this.type_cont = type_cont;
+	}
 }
